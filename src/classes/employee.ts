16727b5831bebe943bb5527795522e71127f8954
shannonMG <shannonmastrogiovanni@gmail.com>
2024-09-30 18:50:49 +0000

    added employee class to employee.ts file

--- src/classes/employee.ts
+++ src/classes/employee.ts
@@ -1,1 +1,18 @@
 //Class for Employee//
+
+class Employee {
+    id: number;
+    first_name: string; Yeah
+    last_name: string;
+    role_id: number;
+    manager_id: number;
+
+    constructor(id: number, first_name: string, last_name: string, role_id: number, manager_id: number){
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.role_id = role_id;
+        this.manager_id = manager_id;
+    }
+}
+
