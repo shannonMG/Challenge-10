7f65b5ccffc141735a384acbc81bd81f1053e6cc
ShannonMG <shannonmastrogiovanni@gmail.com>
2024-09-30 02:53:11 -0400

    updated schemal.sql

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+--This file will set up our three data tables--
+--department table--
+CREATE TABLE departments(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+--role table--
+
+
+--employee table--
